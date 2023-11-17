1025db5ae95d4ccfdbb7a622fed01726424f581d
Raul M <118123543+pillow3@users.noreply.github.com>
2023-11-17 19:56:26 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+// 4GeeksAcademy
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+// My Code
+function domain(){
+    for(let p = 0; p < pronoun.length; p++){
+        for(let a = 0; a < adj.length; a++){
+            for(let n = 0; n < noun.length; n++){
+                console.log(pronoun[p]+adj[a]+noun[n]+'.com')
+        }
+    }
+}
+}
+
+domain()
+
+
+// thegreatjogger.com -- 1
+// thegreatracoon.com -- 2
+// ourgreatjogger.com -- 3
+// ourgreatracoon.com -- 4
+// thebigjogger.com -- 5
+// thebigracoon.com -- 6
+// ourbigjogger.com -- 7
+// ourbigracoon.com -- 8
